b89a8f02f1d98a6b6dffdd6e6ece2be38dc3404d
vladislav.alekseyev <vladislav.alekseyev@p2h.com>
2021-10-27 04:16:34 +0300

    fix test for adding to bucket instead of bucket button

--- test_items.py
+++ test_items.py
@@ -3,4 +3,4 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_bucket_button(browser):
     browser.get(link)
-    browser.find_element_by_class_name('btn-group')
+    browser.find_element_by_css_selector('[type="submit"]')
