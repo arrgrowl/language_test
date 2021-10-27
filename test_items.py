a06c0baf8b020c78227eb86733b0ef5e26ed5d97
vladislav.alekseyev <vladislav.alekseyev@p2h.com>
2021-10-27 04:27:03 +0300

    add another one fix according to task

--- test_items.py
+++ test_items.py
@@ -3,4 +3,5 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_bucket_button(browser):
     browser.get(link)
-    browser.find_element_by_css_selector('[type="submit"]')
+    button = browser.find_element_by_css_selector('[type="submit"]')
+    assert button, 'кнопка не найдена'
